ac81e06e72af9f6306fdefd3a67bc03c75bdb7a5
Rishi Madhok <rishimadhok96@gmail.com>
2024-06-15 12:56:40 -0700

    Added support for moondream

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 # imports
 from app.document_fetcher import DocumentFetcher
+from app.describe_image import DescribeImage
 
 # main
 def main():
     # initialize classes:
     document_fetcher = DocumentFetcher()
+    describe_image = DescribeImage()
     # ...intialize more moduels here
 
     try:
@@ -13,6 +15,10 @@ def main():
 
         # Grab images
         # ...grab and analyze image module here
+        image_path = '/Users/rishimadhok/Desktop/aircraft_carrier.png'
+        img_description = describe_image.invoke(image_path)
+
+        print (img_description)
 
         # Grab aircraft data
         # ...grab aircraft data here
