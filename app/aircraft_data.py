f69502b373bc32437bd9cd10a7048226629d8a43
BMTCompany2 <96481893+BMTCompany2@users.noreply.github.com>
2024-06-15 13:20:35 -0700

    Added static aricraft info

--- app/aircraft_data.py
+++ app/aircraft_data.py
@@ -1,5 +1,10 @@
 # imports
 import json
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
+import re
 
 from datetime import datetime, timedelta, timezone
 from langchain_community.document_loaders import WikipediaLoader
@@ -21,17 +26,17 @@ class AircraftData:
 
         return aircraft_content
     
-    def _generate_aircraft_image(self, aircraft_type):
-        image_path = ""
-
-        return image_path
 
     def _generate_data(self, aircraft_type):
+        print('Getting aircraft data...')
         docs = WikipediaLoader(query=aircraft_type, load_max_docs=1).load()
+
+        print(docs)
+        print()
 
         return docs
     
-    def _generate_aircraft_content(self, aircraft_type):
+    def _generate_aircraft_content(self):
 
         CONFIG_PATH = "app/document_sources/aircraft_mock_data.json"
         aircraft = []
@@ -43,12 +48,15 @@ class AircraftData:
 
         aircraft_content = []
         for craft in aircraft:
+            print(craft)
             aircraft_type = craft['type']
             callsign = craft['callsign']
-            image_path = self._generate_aircraft_image(aircraft_type)
-            aircraft_data = self._generate_aircraft_image(aircraft_type)
+            image_path = craft['img']
+            aircraft_data_doc = self._generate_data(aircraft_type)
 
-            aircraft_content.append(AircraftContent(callsign=callsign, image_path=image_path, aircraft_data=aircraft_data))
+            aircraft_content.append(AircraftContent(callsign=callsign, image_path=image_path, aircraft_data=aircraft_data_doc[0].page_content))
+        
+        print(aircraft_content[0])
             
         return aircraft_content
     
